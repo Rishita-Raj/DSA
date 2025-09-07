48bdc83718fa0ec2c4a48dca9b3d50381d1e9396
Rishita-Raj <148189268+Rishita-Raj@users.noreply.github.com>
2025-09-07 19:50:51 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (13.06%) - LeetSync

---
+++ 1033-broken-calculator/broken-calculator.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+
+int brokenCalc(int startValue, int target) {
+        int res = 0;
+        while(target > startValue){
+            target = target%2 > 0 ? target + 1: target/2;
+            res++;
+        }
+        res += startValue - target;
+        return res;
+    }
+};
