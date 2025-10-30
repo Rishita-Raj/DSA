23599d91d7c18cb58614bf86e219554a5b54d4dd
Rishita-Raj <148189268+Rishita-Raj@users.noreply.github.com>
2025-10-30 10:09:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.32%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        if (num == 0) return 0;         // Base case
+        return 1 + (num - 1) % 9;       // Digital root formula
+    }
+};
