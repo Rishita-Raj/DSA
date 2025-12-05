abffd9a5ea97e565405da5846543d34417a1290e
Rishita-Raj <148189268+Rishita-Raj@users.noreply.github.com>
2025-12-05 10:51:29 +0530

    Time: 0 ms (100.00%) | Memory: 50.1 MB (67.89%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -7,10 +7,10 @@ public:
         for(int i = 0; i<n; i++){
             if(nums[i]==1) {
                 count++;
+             maxi = max(maxi, count); 
             }
             else count = 0; 
 
-             maxi = max(maxi, count); 
         }
     return maxi;
 
