45d6e40feec38ff2b74700c677daf217175767d4
Rishita-Raj <148189268+Rishita-Raj@users.noreply.github.com>
2026-01-10 19:46:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (98.86%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) return false;
+        if(s.length() != t.length()) return false;
 
-        vector<int> freq(26, 0);
+        int freq[26] ={0};
 
-        for (int i = 0; i < s.length(); i++) {
-            freq[s[i] - 'a']++;
-            freq[t[i] - 'a']--;
+        for( int c : s){
+            freq[c-'a']++;
         }
 
-        for (int count : freq) {
-            if (count != 0) return false;
+        for(int c : t){
+            freq[c-'a']--;
         }
 
+        for(int i = 0; i<26; ++i){
+            if(freq[i] != 0){
+                return false;
+                break;
+            }
+        }
         return true;
     }
 };
