eee0808bab229b1f9c585f63b202216b56d5ee3d
Rishita-Raj <148189268+Rishita-Raj@users.noreply.github.com>
2026-03-05 16:31:17 +0530

    Time: 4 ms (81.87%) | Memory: 13.5 MB (25.64%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -5,23 +5,18 @@ public:
 
         vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
 
-        // Base cases
-        for(int i = 0; i <= n; i++) dp[i][0] = i;
-        for(int j = 0; j <= m; j++) dp[0][j] = j;
+        for(int i = 0; i<=n; i++) dp[i][0] = i;
+        for(int j = 0; j<=m; j++) dp[0][j] = j;
 
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= m; j++) {
-
-                if(word1[i-1] == word2[j-1]) 
-                    dp[i][j] = dp[i-1][j-1]; // no operation needed
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                if(word1[i-1] == word2[j-1])
+                    dp[i][j] = dp[i-1][j-1];
                 else
-                    dp[i][j] = 1 + min({ dp[i-1][j],    // delete
-                                        dp[i][j-1],    // insert
-                                        dp[i-1][j-1]   // replace
-                                      });
+                    dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
+                
             }
-        }
-
-        return dp[n][m];
+        } 
+            return dp[n][m];
     }
 };
