d95321529af2cb33c768936e8d290b751e329f61
Rishita-Raj <148189268+Rishita-Raj@users.noreply.github.com>
2026-03-05 17:29:43 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (69.59%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -12,19 +12,17 @@
 class Solution {
 public:
     int i = 0;
-
-    TreeNode* buildBST(vector<int>& preorder, int bound) {
-        if (i == preorder.size() || preorder[i] > bound)
+    TreeNode* buildBST(vector<int>& preorder, int bound){
+        if(i == preorder.size() || preorder[i] > bound)
             return nullptr;
+
         TreeNode* root = new TreeNode(preorder[i++]);
         root->left = buildBST(preorder, root->val);
         root->right = buildBST(preorder, bound);
 
         return root;
     }
-
     TreeNode* bstFromPreorder(vector<int>& preorder) {
         return buildBST(preorder, INT_MAX);
     }
 };
-
