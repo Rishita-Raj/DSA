ad8780bf00535e78085d39d412b8a23afa763c39
Rishita-Raj <148189268+Rishita-Raj@users.noreply.github.com>
2026-04-26 22:31:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (79.84%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,17 +4,15 @@ public:
         int n = nums.size();
         vector<int> answer(n, 1);
 
-        // Step 1: prefix products
         int prefix = 1;
         for (int i = 0; i < n; i++) {
-            answer[i] = prefix;   // product of all left elements
+            answer[i] = prefix;  
             prefix *= nums[i];
         }
 
-        // Step 2: suffix products
         int suffix = 1;
         for (int i = n - 1; i >= 0; i--) {
-            answer[i] *= suffix;  // multiply with all right elements
+            answer[i] *= suffix; 
             suffix *= nums[i];
         }
 
