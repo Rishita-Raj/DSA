85264421f68a81494c31df37cd311352803db17c
Rishita-Raj <148189268+Rishita-Raj@users.noreply.github.com>
2026-05-02 10:19:12 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (68.70%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -8,18 +8,17 @@ public:
                 i--;
             }
             if(i<0) break;
-
             int end = i;
-            while(i>=0 && s[i] !=' '){
+            while(i>=0 && s[i] != ' '){
                 i--;
-            } 
+            }
 
             string word = s.substr(i+1, end-i);
             if(!result.empty()){
                 result += " ";
             }
             result += word;
-        }
+            }
         return result;
     }
 };
