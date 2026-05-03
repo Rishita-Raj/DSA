da9ff53f860e65266f8ee19597e7cf71a58eab5a
Rishita-Raj <148189268+Rishita-Raj@users.noreply.github.com>
2026-05-03 10:32:03 +0530

    Time: 3 ms (31.41%) | Memory: 10.8 MB (29.14%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     vector<vector<int>> result;
     vector<int> path;
-
-    void backtrack(vector<int>& nums, int start) {
-        // Add current subset
+    void backtrack(vector<int>& nums, int start){
         result.push_back(path);
-
-        for (int i = start; i < nums.size(); i++) {
-            // Skip duplicates
-            if (i > start && nums[i] == nums[i - 1])
-                continue;
-
+        for(int i = start; i<nums.size(); i++){
+            if(i>start && nums[i] == nums[i-1])
+            continue;
             path.push_back(nums[i]);
-            backtrack(nums, i + 1);
-            path.pop_back(); // backtrack
+            backtrack(nums,i+1);
+            path.pop_back();
         }
     }
-
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end()); // Important
+        sort(nums.begin(), nums.end());
         backtrack(nums, 0);
         return result;
     }
