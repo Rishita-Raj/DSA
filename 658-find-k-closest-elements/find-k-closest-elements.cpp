1e33f4ebf6551032d1ff317b90844143992bf1cd
Rishita-Raj <148189268+Rishita-Raj@users.noreply.github.com>
2026-06-16 11:03:36 +0530

    Time: 0 ms (100.00%) | Memory: 35.7 MB (92.18%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -4,21 +4,15 @@ public:
         int n = arr.size();
         int low = 0, high = n - k;
 
-        // Binary search for the best starting index
         while (low < high) {
             int mid = low + (high - low) / 2;
 
-            // Compare the distances of the edges of window [mid...mid+k-1]
             if (x - arr[mid] > arr[mid + k] - x) {
-                // Move window to the right
                 low = mid + 1;
             } else {
-                // Keep window or move it to the left
-                high = mid;
+                  high = mid;
             }
         }
-
-        // Return the k closest elements starting from low
         return vector<int>(arr.begin() + low, arr.begin() + low + k);
     }
 };
