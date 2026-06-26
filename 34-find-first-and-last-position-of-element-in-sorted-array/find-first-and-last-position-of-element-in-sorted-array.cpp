bf845f3881392b629cf2ff3eff97aefde1773678
Rishita-Raj <148189268+Rishita-Raj@users.noreply.github.com>
2026-06-26 10:55:04 +0530

    Time: 3 ms (1.37%) | Memory: 18.3 MB (19.01%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -13,10 +13,10 @@ public:
                 }else{
                     left = mid+1;
                 }
-            }else if(nums[mid] < target){
-                left = mid+1;
-            }else{
+            }else if(nums[mid] > target){
                 right = mid-1;
+            }else{
+                left = mid+1;
             }
         }
         return result;
