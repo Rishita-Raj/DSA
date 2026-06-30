f113310d3ca2eeda949bdfe6d58d5e721d005e81
Rishita-Raj <148189268+Rishita-Raj@users.noreply.github.com>
2026-06-30 10:47:43 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (29.26%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -4,7 +4,7 @@ public:
         int n = arr.size();
           int low = 0, high = n - 1;
     while (low <= high) {
-        int mid = (low + high) / 2;
+        int mid = low+(high-low)/2;
         int missing = arr[mid] - (mid + 1);
         if (missing < k) {
             low = mid + 1;
