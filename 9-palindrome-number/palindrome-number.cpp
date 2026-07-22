f3ab08a6df97741ccd84006a216e35ac2cc09bc7
Rishita-Raj <148189268+Rishita-Raj@users.noreply.github.com>
2026-07-22 10:18:23 +0530

    Time: 6 ms (16.29%) | Memory: 8.5 MB (65.61%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        unsigned int temp, reverse = 0;
+        temp = x;
+        while(temp > 0){
+            int last = temp %10;
+            reverse = reverse *10 + last;
+            temp = temp / 10;
+        }
+        return reverse == x;
+    }
+};
