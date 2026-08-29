661c9340d291fb0d9db8b41f39ea60e7b74b7723
Rishita-Raj <148189268+Rishita-Raj@users.noreply.github.com>
2026-08-29 12:30:09 +0530

    Time: 139 ms (85.35%) | Memory: 149.1 MB (83.94%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
-        vector<pair<int,int>> arr;
-        for(int i=0;i<nums.size();i++) arr.push_back({nums[i], i});
-        sort(arr.begin(), arr.end());
-        int left = 0, right = 1;
-        while(right < nums.size()){
-            while(right < nums.size() && arr[right].first - arr[right - 1].first <= limit) right++;
-            vector<int> idx;
-            for(int i=left;i<right;i++){
-                idx.push_back(arr[i].second);
-            }
-            sort(idx.begin(), idx.end());
-            int curridx = left;
-            for(auto x : idx){
-                nums[x] = arr[curridx].first;
-                curridx++;
-            }
-            left = right;
-            right++;
+        int n = nums.size();
+        vector<pair<int, int>> arr;
+
+        for(int i = 0; i < n; i++) {
+            arr.push_back({nums[i], i});
         }
-        return nums;
+        sort(arr.begin(), arr.end());
+
+        vector<int> ans = nums;
+
+        int start = 0;
+
+        while(start < n) {
+
+            int end = start;
+            while(end + 1 < n &&
+                  arr[end + 1].first - arr[end].first <= limit) {
+                end++;
+            }
+            vector<int> indices;
+
+            for(int i = start; i <= end; i++) {
+                indices.push_back(arr[i].second);
+            }
+            sort(indices.begin(), indices.end());
+            for(int i = 0; i < indices.size(); i++) {
+                ans[indices[i]] = arr[start + i].first;
+            }
+
+            start = end + 1;
+        }
+
+        return ans;
     }
 };
