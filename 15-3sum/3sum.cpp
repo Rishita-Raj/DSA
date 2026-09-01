eb82177959745ace92fc3c808c664be96ee4608b
Rishita-Raj <148189268+Rishita-Raj@users.noreply.github.com>
2026-09-01 07:43:06 +0530

    Time: 39 ms (94.63%) | Memory: 29.1 MB (73.19%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -5,16 +5,12 @@ public:
         vector<vector<int>> result;
 
         sort(nums.begin(), nums.end());
-
         for(int i = 0; i<n-2;i++){
             if(i>0 && nums[i] == nums[i-1]) continue;
-
             if(nums[i] > 0) break;
-
             int j = i+1, k = n-1;
             while(j<k){
                 int sum = nums[i] + nums[j] + nums[k];
-
                if(sum == 0){
                     result.push_back({nums[i], nums[j], nums[k]});
                     while(j<k && nums[j] == nums[j+1]) j++;
@@ -27,7 +23,6 @@ public:
                 j++;
                }
             }
-
         }
         return result;
     }
