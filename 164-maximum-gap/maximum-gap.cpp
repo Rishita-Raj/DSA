453b0349d28160dbab580a63d56183f42b0ada42
Rishita-Raj <148189268+Rishita-Raj@users.noreply.github.com>
2026-09-26 08:20:49 +0530

    Time: 42 ms (51.16%) | Memory: 85.9 MB (75.62%) - LeetSync

--- 164-maximum-gap/maximum-gap.cpp
+++ 164-maximum-gap/maximum-gap.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        int n = nums.size();
-        if (n < 2) return 0;
+        
+        if (nums.size() < 2)
+            return 0;
 
-        int minVal = *min_element(nums.begin(), nums.end());
-        int maxVal = *max_element(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());
 
-        int bucketSize = max(1, (maxVal - minVal) / (n - 1));
-        int bucketCount = (maxVal - minVal) / bucketSize + 1;
+        int maxDiff = 0;
 
-        vector<int> bucketMin(bucketCount, INT_MAX);
-        vector<int> bucketMax(bucketCount, INT_MIN);
-        vector<bool> used(bucketCount, false);
-
-        for (int num : nums) {
-            int idx = (num - minVal) / bucketSize;
-            bucketMin[idx] = min(bucketMin[idx], num);
-            bucketMax[idx] = max(bucketMax[idx], num);
-            used[idx] = true;
+        for (int i = 1; i < nums.size(); i++) {
+            maxDiff = max(maxDiff, nums[i] - nums[i - 1]);
         }
 
-        // Find maximum gap
-        int maxGap = 0;
-        int prevMax = minVal;
-
-        for (int i = 0; i < bucketCount; i++) {
-            if (!used[i]) continue;
-            maxGap = max(maxGap, bucketMin[i] - prevMax);
-            prevMax = bucketMax[i];
-        }
-
-        return maxGap;
+        return maxDiff;
     }
 };
